1b2ae707f8182de80bc26aaeb5fded0ccb651fa1
kitsuneProgram <mutyahureai2@gmail.com>
2025-01-03 11:38:13 +0900

    a

--- src/app/api/client.ts
+++ src/app/api/client.ts
@@ -22,8 +22,8 @@ export type News = {
 }
 
 export const client = createClient({
-    serviceDomain:  process.env.NEXT_SERVICEDOMAIN || "",
-    apiKey: process.env.NEXT_APIKEY || "",
+    serviceDomain:  process.env.SERVICEDOMAIN || "service domain",
+    apiKey: process.env.APIKEY || "apikey",
 });
 
 export const getNews = async (queries?: MicroCMSQueries) => {
